4bece552945088a51afb89668e08ca0260d82fbe
morganstar <159751+morganstar@users.noreply.github.com>
2021-05-28 10:26:32 -0700

    Implement router and basic nav layout. Use merge-images to generate app logo.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import mergeImages from 'merge-images'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Switch>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+
+        </Router>
+    );
+}
+
+function Home() {
+    mergeImages(['/assets/body.png', '/assets/eyes.png', '/assets/mouth.png'])
+        .then(b64 => document.querySelector('.App-logo').src = b64)
+
+    return (
+        <div className="App">
+            <header className="App-container">
+                <h1>GENFT</h1>
+                <img src="" className="App-logo" alt="logo" />
+            </header>
+        </div>
+    );
 }
 
 export default App;
