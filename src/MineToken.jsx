9ae8f339f07d1417dc9ddb576b6c6961f834c4ba
morganstar <159751+morganstar@users.noreply.github.com>
2021-05-30 00:04:52 -0700

    MineToken connects to Ethereum and attempts to mint tokens using designated contract instance. Call to mint() failing, possible miss-match of signer address used for finding valid seeds.

--- src/MineToken.jsx
+++ src/MineToken.jsx
@@ -4,18 +4,22 @@ import {Button} from "react-bootstrap";
 import {genftParser} from "./genft-parser";
 import {useParams} from "react-router-dom";
 
-import body from "./assets/body.png";
-import body2 from "./assets/body-02.png";
-import body3 from "./assets/body-03.png";
-import eyes from "./assets/eyes.png";
-import eyes2 from "./assets/eyes-02.png";
-import mouth from "./assets/mouth.png";
-import mouth2 from "./assets/mouth-02.png";
+// import body from "./assets/body.png";
+// import body2 from "./assets/body-02.png";
+// import body3 from "./assets/body-03.png";
+// import eyes from "./assets/eyes.png";
+// import eyes2 from "./assets/eyes-02.png";
+// import mouth from "./assets/mouth.png";
+// import mouth2 from "./assets/mouth-02.png";
 
 import Particles from "react-tsparticles";
 
 import './MineToken.scss'
 import {SAMPLE_GENOME} from "./sampleData";
+import {ethers} from "ethers";
+import genftFactoryDetails from "./abis/GenftFactory.json";
+import genftDetails from "./abis/Genft.json";
+import _ from "lodash";
 
 const TEST_SETTINGS = SAMPLE_GENOME
 
@@ -102,11 +106,82 @@ const backgroundOptions = {
 
 const MineToken = () => {
     const {collectionId} = useParams()
+    const genftFactoryAddress = "0x74AaF8415506AdefD3f267A570fd0dE7d4101eC4"  // TODO: LOCAL DEV SERVER ADDRESS - Replace this with deployed address
     const [isMining, setIsMining] = useState(false)
     const [foundTokens, setFoundTokens] = useState([])
+    const [provider, setProvider] = useState(null)
+    const [signer, setSigner] = useState(null)
+    const [myAddress, setMyAddress] = useState(null)
+    const [genftFactoryContract, setGenftFactoryContract] = useState(null)
+    const [genftContract, setGenftContract] = useState(null)
+    const [genftAddress, setGenftAddress] = useState(null)
+
     const handleFoundToken = async token => {
         if (token && token.dna) {
             setFoundTokens([...foundTokens, {...token, img: await genftParser(token.dna, TEST_SETTINGS)}])
+        }
+    }
+
+    const handleConnectEthereum = async () => {
+        // TODO: Make Ethereum connection usable throughout React app, instead of re-implementing on specific pages
+        console.log("Request to connect to Ethereum")
+        let newProvider
+        let newSigner
+        let signerAddress
+        try {
+            await window.ethereum.enable()
+            newProvider = new ethers.providers.Web3Provider(window.ethereum);
+            console.log("provider:", newProvider)
+            setProvider(newProvider)
+            newSigner = newProvider.getSigner();
+            console.log("signer:", newSigner)
+            setSigner(newSigner)
+            signerAddress = await newSigner.getAddress();
+            setMyAddress(signerAddress)
+        } catch (e) {
+            console.log("ERROR: Connecting to Ethereum wallet: ", e.toString())
+            return
+        }
+
+        // console.log("abi", genftDetails.abi)
+        try {
+            // Setup Genft Factory contract model
+            const factoryContract = new ethers.Contract(genftFactoryAddress, genftFactoryDetails.abi, newProvider)
+            const factoryContractWithSigner = factoryContract.connect(newSigner)
+            setGenftFactoryContract(factoryContractWithSigner)
+
+            let instanceAddress = await factoryContractWithSigner.instances(collectionId)
+            console.log("instanceAddress", instanceAddress)
+            setGenftAddress(instanceAddress)
+
+            // Setup Genft contract model
+            const contract = new ethers.Contract(instanceAddress, genftDetails.abi, newProvider)
+            const contractWithSigner = contract.connect(newSigner)
+            setGenftContract(contractWithSigner)
+
+            contractWithSigner.on("TokenMinted", async (from, dna, event) => {
+                console.log("TokenMinted event detected", from, dna, event)
+                if(from === signerAddress) {
+                    console.log("Detected mint of new Genft token by current user: ", dna)
+                    // TODO: Redirect to token page for newly minted token
+                }
+            })
+        } catch (e) {
+            console.log("ERROR: Using GenftFactory or Genft contract: ", e.toString())
+            return
+        }
+    }
+
+    const handleMintToken = async (seed) => {
+        seed = "0x" + seed
+        console.log("Request to mint token with seed: ", seed)
+        try {
+            // TODO: Set tokenUri to something meaningful (or remove it from the spec if unnecessary)
+            const tokenUri = ""
+            const result = await genftContract.mint(seed, tokenUri)
+            // TODO: Fix whatever is failing with a VM Exception when attempting to run genftContract.mint
+        } catch (e) {
+            console.log("ERROR: Minting token: ", e.toString())
         }
     }
 
@@ -119,26 +194,38 @@ const MineToken = () => {
             />
             }
             <div className='inner'>
-                {!isMining && <Button onClick={() => setIsMining(true)}>Start mining</Button>}
-                {isMining &&
-                <>
-                    <TokenMiner
-                        tokenId={TEST_SETTINGS.tokenId}
-                        difficulty={TEST_SETTINGS.difficulty}
-                        genomeLength={TEST_SETTINGS.genomeLength}
-                        address={TEST_ADDRESS}
-                        onSuccess={handleFoundToken}
-                    />
-                    <Button onClick={() => setIsMining(false)}>Stop mining</Button>
-                </>
-                }
-                <div className='availableTokens'>
-                    {foundTokens.map(token =>
-                        <div key={token.dna} id={`token-${token.dna}`} className='token'>
-                            {token.img.imageData && <img src={token.img.imageData}/>}
+
+                {_.isNull(signer) && (
+                    <button onClick={handleConnectEthereum}>
+                        Connect Wallet
+                    </button>
+                )}
+                {!_.isNull(signer) && (
+                    <>
+                        {!isMining && <Button onClick={() => setIsMining(true)}>Start mining</Button>}
+                        {isMining &&
+                        <>
+                            <TokenMiner
+                                tokenId={TEST_SETTINGS.tokenId}
+                                difficulty={TEST_SETTINGS.difficulty}
+                                genomeLength={TEST_SETTINGS.genomeLength}
+                                // address={TEST_ADDRESS}
+                                address={myAddress}
+                                onSuccess={handleFoundToken}
+                            />
+                            <Button onClick={() => setIsMining(false)}>Stop mining</Button>
+                        </>
+                        }
+                        <div className='availableTokens'>
+                            {foundTokens.map(token =>
+                                <div key={token.dna} id={`token-${token.dna}`} className='token' onClick={() => {handleMintToken(token.seed, "")}}>
+                                    {token.img.imageData && <img src={token.img.imageData} title={`DNA: ${token.dna}`} />}
+                                </div>
+                            )}
                         </div>
-                    )}
-                </div>
+                    </>
+                )}
+
             </div>
         </div>
     )
