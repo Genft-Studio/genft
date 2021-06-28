d4dae612554619c0f70f1ce72c6377c5e5bad6ca
bgok <ken@citizenhodler.com>
2021-06-28 14:51:23 -0700

    Define menu site nav structure

--- src/App.js
+++ src/App.js
@@ -1,49 +1,55 @@
 import './App.scss';
 import {
-    HashRouter as Router,
+    BrowserRouter as Router,
     Switch,
     Route,
-    Link
 } from "react-router-dom";
 import mergeImages from 'merge-images'
 import TokenPage from "./TokenPage";
 import FactoryFactory from "./FactoryFactory";
 import MineToken from "./MineToken";
+import {Nav, Navbar, NavDropdown} from "react-bootstrap";
 
 function App() {
     return (
         <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/factory-factory">Factory Factory</Link>
-                        </li>
-                        <li>
-                            <Link to="/token">Token</Link>
-                        </li>
-                    </ul>
-                </nav>
-
-                <Switch>
-                    <Route path="/factory-factory" exact>
-                        <FactoryFactory />
-                    </Route>
-                    <Route path="/token" exact>
-                        <TokenPage />
-                    </Route>
-                    <Route path="/:collectionId" exact>
-                        <MineToken />
-                    </Route>
-                    <Route path="/" exact>
-                        <Home />
-                    </Route>
-                </Switch>
-            </div>
-
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="/">GenFT Studio</Navbar.Brand>
+                <Navbar id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <NavDropdown title="Studios">
+                            <NavDropdown.Item href="/studio/pixel-editor">Pixel Editor</NavDropdown.Item>
+                            <NavDropdown.Item href="/studio/layered-images">Image</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="/collections">Collections</Nav.Link>
+                        <Nav.Link href="/123/456">View Token</Nav.Link>
+                    </Nav>
+                </Navbar>
+            </Navbar>
+            <Switch>
+                <Route path="/" exact>
+                    <Home/>
+                </Route>
+                <Route path="/studio/layered-images" exact>
+                    <FactoryFactory/>
+                </Route>
+                <Route path="/studio/pixel-editor" exact>
+                    <h1>Pixel editor studio</h1>
+                </Route>
+                <Route path="/:collectionId" exact>
+                    <h1>Collection Gallery</h1>
+                </Route>
+                <Route path="/:collectionId/mine" exact>
+                    <MineToken/>
+                </Route>
+                <Route path="/:collectionId/:tokenId" exact>
+                    <TokenPage/>
+                </Route>
+                <Route path="/collections">
+                    <h1>List of collections</h1>
+                </Route>
+            </Switch>
         </Router>
     );
 }
@@ -56,7 +62,7 @@ function Home() {
         <div className="App">
             <header className="App-container">
                 <h1>GENFT</h1>
-                <img src="" className="App-logo" alt="logo" />
+                <img src="" className="App-logo" alt="logo"/>
             </header>
         </div>
     );
